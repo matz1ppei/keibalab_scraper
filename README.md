4857587996b8e30af1e173ec9aade2f341fac4a1
Ippei Matsubara <matz1ppei@gmail.com>
2020-02-08 14:22:40 +0900

    rewrite functions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # keibalab_scraper
+crawlering & scraping [Keibalab](https://www.keibalab.jp/)
+
+## Usage
+### Initialize
+```
+pipenv install
+```
+### Crawling
+```
+python spider/keibalab_spider.py <from_date> <to_date>
+```
+$python spider/keibalab_spider.py 20191201 20191231
+
+### Scraping
+```
+python scraper/keibalab_scraper.py
+```
--- scraper/keibalab_scraper.py
+++ scraper/keibalab_scraper.py
@@ -3,11 +3,20 @@ import sqlite3
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-RESULT_SELECTOR = 'div.RaceTableWrap > table '
-PAYOFF_SELECTOR = 'div.haraimodoshi > table '
+COURSE_NAME = {
+    '01': '札幌',
+    '02': '函館',
+    '03': '福島',
+    '04': '新潟',
+    '05': '東京',
+    '06': '中山',
+    '07': '中京',
+    '08': '京都',
+    '09': '阪神',
+    '10': '小倉'
+}
 
 def main():
-    args = sys.argv
 
     try:
         conn = sqlite3.connect('./db/keibalab.db')
@@ -17,7 +26,8 @@ def main():
         for html in get_html('./html'):
             print(html.name, flush=True)
             race_info = scrape(html)
-            save(c, race_info)
+            if len(race_info) > 0:
+                save(c, race_info)
 
         conn.commit()
 
@@ -25,15 +35,30 @@ def main():
         conn.close()
 
 def init_db(c):
-    init_course(c)
-    init_result(c)
-    init_payoff(c)
+    create_table_course(c)
+    create_table_result(c)
+    create_table_payoff(c)
 
+def create_table_course(c):
+    c.execute('DROP TABLE IF EXISTS course')
+    c.execute("""
+        CREATE TABLE course (
+            id integer primary key,
+            race_id text,
+            date text,
+            racecourse text,
+            number text,
+            name text,
+            grade text,
+            type text,
+            distance text,
+            weather text,
+            going text,
+            number_of_horses text
+        )
+    """)
 
-def init_course(c):
-    pass
-
-def init_result(c):
+def create_table_result(c):
     c.execute('DROP TABLE IF EXISTS result')
     c.execute("""
         CREATE TABLE result (
@@ -42,10 +67,12 @@ def init_result(c):
             ranking text,
             bracket_no text,
             horse_no text,
+            horse_id text,
             horse_name text,
             sex text,
             age text,
             weight text,
+            jockey_id text,
             jockey text,
             favorite text,
             win_odds text,
@@ -53,12 +80,13 @@ def init_result(c):
             margin text,
             position text,
             final_600 text,
+            trainer_id text,
             trainer text,
             horse_weight text
         )
     """)
 
-def init_payoff(c):
+def create_table_payoff(c):
     c.execute('DROP TABLE IF EXISTS payoff')
     c.execute("""
         CREATE TABLE payoff (
@@ -89,64 +117,136 @@ def scrape(html):
     with open(html, 'r', encoding='utf-8') as f:
         soup = BeautifulSoup(f, 'html.parser')
         race_info = {}
-        race_info['course'] = scrape_course(soup.find('div', class_='racedatabox'))
-        race_info['result'] = scrape_result(soup.find('table', class_='resulttable'))
 
-        """
-        race_info = {
-            'result': {
-                'race_id': 'R' + html.name,
-                'ranking': soup.select_one(RESULT_SELECTOR + 'tr:nth-child(1) > td:nth-child(1)').get_text(),
-                'bracket_no': soup.select_one(RESULT_SELECTOR + 'tr:nth-child(1) > td:nth-child(2)').get_text(),
-                'horse_no': soup.select_one(RESULT_SELECTOR + 'tr:nth-child(1) > td:nth-child(3)').get_text()
-            },
-            'payoff': {
-                'race_id': 'R' + html.name,
-                'win_r': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(1) > td:nth-child(2)').get_text(),
-                'win_p': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(1) > td:nth-child(3)').get_text(),
-                'show_r': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(2) > td:nth-child(2)').get_text(),
-                'show_p': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(2) > td:nth-child(3)').get_text(),
-                'bracketq_r': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(3) > td:nth-child(2)').get_text(),
-                'bracketq_p': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(3) > td:nth-child(3)').get_text(),
-                'quinella_r': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(4) > td:nth-child(2)').get_text(),
-                'quinella_p': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(4) > td:nth-child(3)').get_text(),
-                'exacta_r': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(1) > td:nth-child(5)').get_text(),
-                'exacta_p': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(1) > td:nth-child(6)').get_text(),
-                'trio_r': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(3) > td:nth-child(5)').get_text(),
-                'trio_p': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(3) > td:nth-child(6)').get_text(),
-                'trifecta_r': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(4) > td:nth-child(5)').get_text(),
-                'trifecta_p': soup.select_one(PAYOFF_SELECTOR + 'tr:nth-child(4) > td:nth-child(6)').get_text()
-            }
-        }
-        """
+        result_table = soup.find('table', class_='resulttable')
+        if not result_table is None:
+            course_table = soup.find('div', class_='racedatabox')
+            race_info['course'] = scrape_course(course_table, html.name)
 
-        return race_info
+            race_info['result'] = scrape_result(result_table, html.name)
 
-def scrape_course(table):
-    pass
+            payoff_table = soup.find('div', class_='haraimodoshi').table
+            race_info['payoff'] = scrape_payoff(payoff_table, html.name)
 
-def scrape_result(table):
-    for row in table.findAll('tr'):
-        for column in row.findAll('td'):
-            pass
+    return race_info
 
-def scrape_payoff(table):
-    pass
+def scrape_course(table, file_name):
+    course = {'race_id': 'R'+file_name}
+    type_distance_number = table.find('ul', class_='classCourseSyokin').find_all('li')[1].text
+    name_grade = table.h1.text.strip().replace('\t', '').split('(')
+    course['date'] = get_date(table.p.text)
+    course['racecourse'] = COURSE_NAME[file_name[8:10]]
+    course['number'] = file_name[10:12]
+    # ToDo add conditions?
+    course['name'] = name_grade[0]
+    if len(name_grade) == 1:
+        course['grade'] = ''
+    else:
+        course['grade'] = name_grade[1].split(')')[0]
+    course['type'] = type_distance_number[0:1]
+    course['distance'] = type_distance_number[1:5]
+    course['weather'] = table.ul.find_all('li')[0].text
+    course['going'] = table.ul.find_all('li')[1].text
+    course['number_of_horses'] = type_distance_number[7:9].split('頭')[0]
+    return course
 
+def get_date(text):
+    date_text = text.split('\n')[0]
+    return date_text.split('(')[0]
+
+def scrape_result(table, file_name):
+    result = []
+    for i, row in enumerate(table.find_all('tr')):
+        # print(row)
+        if i > 0:
+            columns = row.find_all('td')
+            result.append({
+                'race_id': 'R'+file_name,
+                'ranking': columns[0].text,
+                'bracket_no': columns[1].text,
+                'horse_no': columns[2].text,
+                'horse_id': columns[3].a.get('href').split('/')[3],
+                'horse_name': columns[3].text,
+                'sex': columns[4].text[0:1],
+                'age': columns[4].text[1:2],
+                'weight': columns[5].text,
+                'jockey_id': columns[6].a.get('href').split('/')[3],
+                'jockey': columns[6].text,
+                'favorite': columns[7].text,
+                'win_odds': columns[8].text.strip(),
+                'finish': columns[9].text,
+                'margin': columns[10].text.strip(),
+                'position': columns[11].text.strip(),
+                'final_600': columns[12].text,
+                'trainer_id': columns[13].a.get('href').split('/')[3],
+                'trainer': columns[13].text,
+                'horse_weight': columns[14].text
+            })
+    return result
+
+def scrape_payoff(table, file_name):
+    payoff = {'race_id': 'R'+file_name}
+    for i, row in enumerate(table.find_all('tr')):
+        columns = row.find_all('td')
+        if i == 0:
+            payoff['win_r'] = columns[1].text
+            payoff['win_p'] = columns[2].text
+            payoff['exacta_r'] = columns[4].text
+            payoff['exacta_p'] = columns[5].text
+        elif i == 1:
+            payoff['show_r'] = columns[1].text
+            payoff['show_p'] = columns[2].text
+            payoff['qplace_r'] = columns[4].text
+            payoff['qplace_p'] = columns[5].text
+        elif i == 2:
+            payoff['bracketq_r'] = columns[1].text
+            payoff['bracketq_p'] = columns[2].text
+            payoff['trio_r'] = columns[4].text
+            payoff['trio_p'] = columns[5].text
+        elif i == 3:
+            payoff['quinella_r'] = columns[1].text
+            payoff['quinella_p'] = columns[2].text
+            payoff['trifecta_r'] = columns[4].text
+            payoff['trifecta_p'] = columns[5].text
+
+    return payoff
 
 def save(c, race_info):
-    save_course(c, race_info['course'])
-    save_result(c, race_info['result'])
-    save_payoff(c, race_info['payoff'])
+    insert_course(c, race_info['course'])
+    insert_result(c, race_info['result'])
+    insert_payoff(c, race_info['payoff'])
 
-def save_course(c, course):
+def insert_course(c, course):
     c.execute("""
-
+        INSERT INTO course (
+            race_id,
+            date,
+            racecourse,
+            number,
+            name,
+            grade,
+            type,
+            distance,
+            weather,
+            going,
+            number_of_horses
+        )
+        VALUES (
+            :race_id,
+            :date,
+            :racecourse,
+            :number,
+            :name,
+            :grade,
+            :type,
+            :distance,
+            :weather,
+            :going,
+            :number_of_horses
+        )
     """, course)
 
-    pass
-
-def save_result(c, result):
+def insert_result(c, result):
     for r in result:
         c.execute("""
            INSERT INTO result (
@@ -154,10 +254,12 @@ def save_result(c, result):
             ranking,
             bracket_no,
             horse_no,
+            horse_id,
             horse_name,
             sex,
             age,
             weight,
+            jockey_id,
             jockey,
             favorite,
             win_odds,
@@ -165,6 +267,7 @@ def save_result(c, result):
             margin,
             position,
             final_600,
+            trainer_id,
             trainer,
             horse_weight
            )
@@ -173,10 +276,12 @@ def save_result(c, result):
             :ranking,
             :bracket_no,
             :horse_no,
+            :horse_id,
             :horse_name,
             :sex,
             :age,
             :weight,
+            :jockey_id,
             :jockey,
             :favorite,
             :win_odds,
@@ -184,12 +289,13 @@ def save_result(c, result):
             :margin,
             :position,
             :final_600,
+            :trainer_id,
             :trainer,
             :horse_weight
            ) 
         """,r)
 
-def save_payoff(c, payoff):
+def insert_payoff(c, payoff):
     c.execute("""
         INSERT INTO payoff (
             race_id,
