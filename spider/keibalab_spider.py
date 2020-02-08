2b38593e44d2359a84e400ef8c3f620774db102d
Ippei Matsubara <matz1ppei@gmail.com>
2020-02-09 01:30:11 +0900

    speeding up

--- spider/keibalab_spider.py
+++ spider/keibalab_spider.py
@@ -1,8 +1,22 @@
 import sys
 import time
 import requests
+from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from pathlib import Path
+
+COURSE_ID = {
+    '札幌': '01',
+    '函館': '02',
+    '福島': '03',
+    '新潟': '04',
+    '東京': '05',
+    '中山': '06',
+    '中京': '07',
+    '京都': '08',
+    '阪神': '09',
+    '小倉': '10'
+}
 
 def main():
     args = sys.argv
@@ -15,23 +29,33 @@ def main():
     else:
         to_date = args[2]
 
-    for url, race_id in generate_urls(start_url, from_date, to_date):
-        time.sleep(1)
-        response = fetch(url)
-        save(response, race_id)
+    for race in check_race(start_url, from_date, to_date):
+        for url, race_id in generate_urls(start_url, race):
+            time.sleep(1)
+            response = fetch(url)
+            save(response, race_id)
 
-def generate_urls(start_url, from_date_string, to_date_string):
-    from_date = datetime.strptime(from_date_string, '%Y%m%d')
-    to_date   = datetime.strptime(to_date_string, '%Y%m%d')
+def check_race(start_url, from_date_str, to_date_str):
+    from_date = datetime.strptime(from_date_str, '%Y%m%d')
+    to_date   = datetime.strptime(to_date_str, '%Y%m%d')
 
-    # 8 racecourses, 12 races
     while(from_date <= to_date):
-        for racecourse in range(1, 11):
-            for racenumber in range(1, 13):
-                race_id = from_date.strftime('%Y%m%d') + f'{racecourse:02}' + f'{racenumber:02}'
-                url = start_url + race_id + '/'
-                yield url, race_id
+        response = fetch(start_url + from_date.strftime('%Y%m%d'))
+        soup = BeautifulSoup(response.text, 'html.parser')
+        if not soup.table is None:
+            print(response.url + '/', flush=True)
+            for table in soup.find_all('table'):
+                cid = COURSE_ID[table.th.text[2:4]]
+                yield from_date.strftime('%Y%m%d') + cid
+        else:
+            time.sleep(1)
         from_date += timedelta(days=1)
+
+def generate_urls(start_url, race):
+    for racenumber in range(1, 13):
+        race_id = race + f'{racenumber:02}'
+        url = start_url + race_id + '/'
+        yield url, race_id
 
 def fetch(url):
     return requests.get(url)
